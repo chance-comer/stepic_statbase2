86ea51ca055661a91111e82c8d3ff969ab411fcc
Natalie <s4astlivii_4elovek@mail.ru>
2018-07-06 23:57:24 +0300

    step4

--- step4.py
+++ step4.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jul  6 15:22:51 2018
+Created on Fri Jul  6 23:40:40 2018
 
-@author: kazantseva
+@author: Nataliya
 """
 
 import pandas as pd
+import numpy as np
 
 data = pd.read_csv('iris.csv')
 
+data = pd.DataFrame({'V1' : [16, 21, 18], \
+                     'V2' : [17, 7, 16], \
+                     'V3' : [25, 23, 27], \
+                     'V4' : [20, 22, 18], \
+                     'V5' : [16, 17, 19]})
+
 means = data.mean(axis = 0)
-
-r = [ row[col] > means[col] for index, row in data.iterrows() for col in means.keys() ]
-
-#data['new_factor'] = ['No' for i in data.iterrows()]
+hals_factors_count = np.ceil(len(means) / 2)
 
 ret = {}
 
@@ -22,10 +26,10 @@ for index, row in data.iterrows():
     for col in means.keys():
         if row[col] > means[col]:
             k += 1
-            if k == 3:
+            if k == hals_factors_count:
                 ret[index] = 'Yes'
                 break
-    if k < 3:
+    if k < hals_factors_count:
         ret[index] = 'No'
 
 
